2f4a9bf9ed4953268a803b8c5fa69d8e676c9224
pollitosabroson <karnal02@gmail.com>
2015-08-24 15:23:51 -0500

    Integration view for angular

--- testapi/settings.py
+++ testapi/settings.py
@@ -88,4 +88,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/assets/'
+
+STATICFILES_DIRS = (
+    os.path.realpath(os.path.join(BASE_DIR, 'assets')),
+)
+
+TEMPLATE_DIRS = (
+    os.path.realpath(os.path.join(BASE_DIR, 'templates')),
+)
--- testapi/urls.py
+++ testapi/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'testapi.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+from . import views
 
+urlpatterns = patterns(
+    '',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('api.urls'))
+    url(r'^api/', include('api.urls')),
+    url(r'^.*', views.AngularAppView.as_view(), name='app')
 )
